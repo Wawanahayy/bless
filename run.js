2c00166418181022b8dcdc09fe451da4e813b149
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-11-15 15:29:01 +0700

    Update run.js

--- run.js
+++ run.js
@@ -192,7 +192,7 @@ async function runAll() {
         setInterval(async () => {
             console.log(`[${new Date().toISOString()}] Sending ping...`);
             await pingNodeWithRetry(nodeId); // Menggunakan retry tanpa batas percobaan dalam interval
-        }, 150000);  // Mengirim ping setiap 60 detik
+        }, 60000);  // Mengirim ping setiap 60 detik
     } catch (error) {
         console.error(`[${new Date().toISOString()}] An error occurred:`, error);
     }
