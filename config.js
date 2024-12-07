1ce21369cd33e8589c341afa68d5119ec5ecc935
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-12-07 22:09:23 +0000

    Memperbaiki bug di halaman login

--- config.js
+++ config.js
@@ -1,14 +1,23 @@
 module.exports = [
     {
-        usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzM1ZjkwZTQ4NzM4Yjc0MDBiZjI1NTciLCJwdWJsaWNBZGRyZXNzIjoiNlVLRXdaRnJ4ckJqQWZLNDNmWjV3ell6aVlOWGV5dm1Oc3ZFS29mcmtLSHoiLCJ3YWxsZXRUeXBlIjoic29sYW5hIiwiaWF0IjoxNzMxNTkwNDE2LCJleHAiOjE3NjMxNDgwMTZ9.QnKUxU_LQBtLavOmECXSvTZrat8srr-vqFlHLgWNImU',
+        usertoken: 'usertoken1',
         nodes: [
-            { nodeId: '12D3KooWDoadxYwdogFnkvfyhiaCydjkgYrLDPvj3dv9yBQoJxKg', hardwareId: '5d19ce2c96dd3ff2b57c74f152995fa876a68cec20979cc10e65cb9077cf80c2', proxy: 'socks5://ud408bcbc55a105b5-zone-custom-region-id-st-centraljava-city-sragen:timplexz@43.152.113.55:2333' }
+            { nodeId: 'nodeid1', hardwareId: 'hardwareid1', proxy: 'proxy1' },
+            { nodeId: 'nodeid2', hardwareId: 'hardwareid2', proxy: 'proxy2' },
+            { nodeId: 'nodeid3', hardwareId: 'hardwareid3', proxy: 'proxy3' },
+            { nodeId: 'nodeid4', hardwareId: 'hardwareid4', proxy: 'proxy4' },
+            { nodeId: 'nodeid5', hardwareId: 'hardwareid5', proxy: 'proxy5' }
         ]
     },
     {
-        usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzM2MGRjMTQ4NzM4Yjc0MDA3ODQ4ZDYiLCJwdWJsaWNBZGRyZXNzIjoiOVNRODJRZGp6MW42QTV2NThLejRGZGlCN3BlVG5vaE5oOVBxVHJpV1pMaEwiLCJ3YWxsZXRUeXBlIjoic29sYW5hIiwiaWF0IjoxNzMzNTcyMjgwLCJleHAiOjE3NjUxMjk4ODB9.45kY7-Of5za2A1lQAUfXua3X_tVyaaINhC-_4t1Cwr8',
+        usertoken: 'usertoken2',
         nodes: [
-            { nodeId: '12D3KooWMDJw3bzv4aMFEMDkNkCe3da5ku24uTKZhki38cKrjUTP', hardwareId: '5d19ce2c96dd3ff2b57c74f152995fa876a68cec20979cc10e65cb9077cf80c2', proxy: 'socks5://ud408bcbc55a105b5-zone-custom-region-id-st-centraljava-city-sragen:timplexz@43.152.113.55:2333' }
+            { nodeId: 'nodeid6', hardwareId: 'hardwareid6', proxy: 'proxy6' },
+            { nodeId: 'nodeid7', hardwareId: 'hardwareid7', proxy: 'proxy7' },
+            { nodeId: 'nodeid8', hardwareId: 'hardwareid8', proxy: 'proxy8' },
+            { nodeId: 'nodeid9', hardwareId: 'hardwareid9', proxy: 'proxy9' },
+            { nodeId: 'nodeid10', hardwareId: 'hardwareid10', proxy: 'proxy10' }
         ]
     }
+    // Add more usertokens as needed
 ];
